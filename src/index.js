a5d8db19a23cb609a7c26ff5986ed80ae17e7f02
Tim Weightman <timweightman@gmail.com>
2015-06-14 20:53:53 +1000

    feat(basic input): Removed the example stuff from master. Added a super basic input example using m.mount.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 'use strict';
 var m = require('mithril');
-var domready = require('domready');
-var routes = require('./routes');
 
-// We need to define the mode that we want to use for all
-// of the routing in our application.
-// It is possible to use search, hash or pathname.
-m.route.mode = 'hash';
+require('./style.css');
 
-domready(function() {
+// Most basic of all.
+var homeComponent = {
+  controller: function() {
+    return {
+      input: m.prop('Home')
+    };
+  },
+  view: function(ctrl) {
+    return m('div', [
+      m('input', {oninput: m.withAttr('value', ctrl.input), value: ctrl.input()}),
+      m('h1', ctrl.input())
+    ]);
+  }
+};
 
-  // This kicks off our applicaiton.
-  // The routes that have been defined will control what gets
-  // rendered into document, and the application will start
-  // at the root '/' route, which will need to have been
-  // defined in the routes variable.
-  m.route(document, '/', routes);
-});
+m.mount(document.querySelector('#mithril-demo'), homeComponent);
