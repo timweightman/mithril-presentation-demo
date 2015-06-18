5b01bedab415a02bbb8f8f5589ca0690dbfd23b0
Tim Weightman <timweightman@gmail.com>
2015-06-18 19:12:54 +1000

    feat(routing): Added routing, filter parameter, and routing to other pages, as well as more link components.

--- src/index.js
+++ src/index.js
@@ -19,15 +19,43 @@ var input = {
   }
 };
 
+var navLink = {
+  controller: function(args) {
+    return {
+      activeClass: m.route() === args.href ? ' active' : ''
+    };
+  },
+  view: function(ctrl, args) {
+    return m('a', {
+      'class': (args.classes || '') + ctrl.activeClass,
+      href: args.href,
+      config: m.route
+    }, args.content || args.href);
+  }
+};
+
+var navigation = {
+  view: function() {
+    return m('div', [
+      m.component(navLink, {classes: 'nav-link', href: '/', content: 'Default Home'}),
+      m.component(navLink, {classes: 'nav-link', href: '/completed', content: 'Filter = "completed"'}),
+      m.component(navLink, {classes: 'nav-link', href: '/about', content: 'About'})
+    ]);
+  }
+};
+
 // Most basic of all.
 var homeComponent = {
   controller: function() {
+    var filterValue = m.route.param('filter') || '';
+
     return {
-      input: m.prop('Home')
+      input: m.prop(filterValue)
     };
   },
   view: function(ctrl) {
     return m('div', [
+      m.component(navigation),
       m.component(input, {
         label: 'Filter',
         bind: ctrl.input,
@@ -40,4 +68,18 @@ var homeComponent = {
   }
 };
 
-m.mount(document.querySelector('#mithril-demo'), homeComponent);
+var aboutComponent = {
+  view: function() {
+    return m('div', [
+      m.component(navigation),
+      m('p', 'This is a demo project that shows step-by-step how common web problems are solved using Mithril.')
+    ]);
+  }
+};
+
+m.route.mode = 'hash';
+
+m.route(document.querySelector('#mithril-demo'), '/from query param!', {
+  '/:filter': homeComponent,
+  '/about': aboutComponent
+});
