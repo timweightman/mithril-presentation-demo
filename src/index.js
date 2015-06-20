25d0e083d53cf8e269f5c1e3cfba75ad2232ea4d
Tim Weightman <timweightman@gmail.com>
2015-06-20 15:46:14 +1000

    feat(page templating): Introduced a page template that wraps the necessary navigation and footer content, and allows different content components to be rendered into it.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-'use strict';
-var m = require('mithril');
+'use strict'
+var m = require('mithril')
 
-require('./style.css');
+require('./style.css')
 
 var input = {
   view: function(ctrl, args) {
-    var inputAttributes = args.input || {};
+    var inputAttributes = args.input || {}
 
     return m('div', [
       m('label', args.label || ''),
@@ -15,22 +15,22 @@ var input = {
         name: inputAttributes.name || '',
         type: inputAttributes.type || 'text'
       })
-    ]);
+    ])
   }
-};
+}
 
 var navLink = {
   controller: function(args) {
     return {
       isActive: m.route() === args.href
-    };
+    }
   },
   view: function(ctrl, args) {
     if (ctrl.isActive) {
       // Render a span if this is the active route.
       return m('span', {
         'class': args.classes + ' active'
-      }, args.content || args.href);
+      }, args.content || args.href)
     }
 
     // Render a link if this is not the active route.
@@ -38,9 +38,9 @@ var navLink = {
       'class': (args.classes || ''),
       href: args.href,
       config: m.route
-    }, args.content || args.href);
+    }, args.content || args.href)
   }
-};
+}
 
 var navigation = {
   view: function() {
@@ -48,22 +48,21 @@ var navigation = {
       m.component(navLink, {classes: 'nav-link', href: '/', content: 'Home'}),
       m.component(navLink, {classes: 'nav-link', href: '/completed', content: 'Completed'}),
       m.component(navLink, {classes: 'nav-link', href: '/about', content: 'About'})
-    ]);
+    ])
   }
-};
+}
 
 // Most basic of all.
 var homeComponent = {
   controller: function() {
-    var filterValue = m.route.param('filter') || '';
+    var filterValue = m.route.param('filter') || ''
 
     return {
       input: m.prop(filterValue)
-    };
+    }
   },
   view: function(ctrl) {
     return m('div', [
-      m.component(navigation),
       m.component(input, {
         label: 'Filter',
         bind: ctrl.input,
@@ -72,23 +71,59 @@ var homeComponent = {
         }
       }),
       m('h1', ctrl.input())
-    ]);
+    ])
   }
-};
+}
 
 var aboutComponent = {
   view: function() {
     return m('div', [
-      m.component(navigation),
       m('p', 'This is a demo project that shows step-by-step how common web problems are solved using Mithril.')
-    ]);
+    ])
   }
-};
+}
 
-m.route.mode = 'hash';
+var templateView = function(ctrl, args, views) {
+  return m('html', [
+    m('head', [
+      m('title', 'Mithril Demo')
+    ]),
+    m('body', [
+      m.component(navigation),
+      views.content,
+      m('footer', 'Footer content'),
+      m.trust('<script src="index.js"></script>')
+    ])
+  ])
+}
+
+var templateMixin = function(templateView, views) {
+  return function(ctrl, args) {
+    var renderedContent = {}
+    Object.keys((views || {})).forEach(function(key) {
+      renderedContent[key] = views[key](ctrl, args)
+    })
+
+    return templateView(ctrl, args, renderedContent)
+  }
+}
+
+var pageTemplate = function(component) {
+  return {
+    controller: component.controller,
+    view: templateMixin(templateView, {
+      content: component.view
+    })
+  }
+}
+
+var pageHome = pageTemplate(homeComponent)
+var pageAbout = pageTemplate(aboutComponent)
+
+m.route.mode = 'hash'
 
 m.route(document.querySelector('#mithril-demo'), '/', {
-  '/': homeComponent,
-  '/:filter': homeComponent,
-  '/about': aboutComponent
-});
+  '/': pageHome,
+  '/:filter': pageHome,
+  '/about': pageAbout
+})
