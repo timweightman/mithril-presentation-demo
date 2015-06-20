4cfecd295b19cc73c377886fa41422e2453dd8eb
Tim Weightman <timweightman@gmail.com>
2015-06-20 15:51:55 +1000

    chore(semicolon): stripped all semicolons as they are not required, for consistency with other branches, and less risk for live-coding.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-'use strict';
-var m = require('mithril');
+'use strict'
+var m = require('mithril')
 
-require('./style.css');
+require('./style.css')
 
 var input = {
   view: function(ctrl, args) {
-    var inputAttributes = args.input || {};
+    var inputAttributes = args.input || {}
 
     return m('div', [
       m('label', args.label || ''),
@@ -15,22 +15,22 @@ var input = {
         name: inputAttributes.name || '',
         type: inputAttributes.type || 'text'
       })
-    ]);
+    ])
   }
-};
+}
 
 var navLink = {
   controller: function(args) {
     return {
       isActive: m.route() === args.href
-    };
+    }
   },
   view: function(ctrl, args) {
     if (ctrl.isActive) {
       // Render a span if this is the active route.
       return m('span', {
         'class': args.classes + ' active'
-      }, args.content || args.href);
+      }, args.content || args.href)
     }
 
     // Render a link if this is not the active route.
@@ -38,9 +38,9 @@ var navLink = {
       'class': (args.classes || ''),
       href: args.href,
       config: m.route
-    }, args.content || args.href);
+    }, args.content || args.href)
   }
-};
+}
 
 var navigation = {
   view: function() {
@@ -48,18 +48,18 @@ var navigation = {
       m.component(navLink, {classes: 'nav-link', href: '/', content: 'Home'}),
       m.component(navLink, {classes: 'nav-link', href: '/completed', content: 'Completed'}),
       m.component(navLink, {classes: 'nav-link', href: '/about', content: 'About'})
-    ]);
+    ])
   }
-};
+}
 
 // Most basic of all.
 var homeComponent = {
   controller: function() {
-    var filterValue = m.route.param('filter') || '';
+    var filterValue = m.route.param('filter') || ''
 
     return {
       input: m.prop(filterValue)
-    };
+    }
   },
   view: function(ctrl) {
     return m('div', [
@@ -72,23 +72,23 @@ var homeComponent = {
         }
       }),
       m('h1', ctrl.input())
-    ]);
+    ])
   }
-};
+}
 
 var aboutComponent = {
   view: function() {
     return m('div', [
       m.component(navigation),
       m('p', 'This is a demo project that shows step-by-step how common web problems are solved using Mithril.')
-    ]);
+    ])
   }
-};
+}
 
-m.route.mode = 'hash';
+m.route.mode = 'hash'
 
 m.route(document.querySelector('#mithril-demo'), '/', {
   '/': homeComponent,
   '/:filter': homeComponent,
   '/about': aboutComponent
-});
+})
