dbc23d9ae71f8668d96511d6418adb760845a952
Tim Weightman <timweightman@gmail.com>
2015-06-20 15:48:06 +1000

    chore(semicolon): stripped all semicolons as they are not required, for consistency with other branches, and less risk for live-coding.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-'use strict';
-var m = require('mithril');
+'use strict'
+var m = require('mithril')
 
-require('./style.css');
+require('./style.css')
 
 // Most basic of all.
 var homeComponent = {
   controller: function() {
     return {
       input: m.prop('Home')
-    };
+    }
   },
   view: function(ctrl) {
     return m('div', [
       m('input', {oninput: m.withAttr('value', ctrl.input), value: ctrl.input()}),
       m('h1', ctrl.input())
-    ]);
+    ])
   }
-};
+}
 
-m.mount(document.querySelector('#mithril-demo'), homeComponent);
+m.mount(document.querySelector('#mithril-demo'), homeComponent)
